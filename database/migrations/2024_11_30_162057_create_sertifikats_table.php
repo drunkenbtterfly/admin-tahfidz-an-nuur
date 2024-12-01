ff3c6a6da379788ce2ea25c97957198b6dcfa032
irfan <143263391+drunkenbtterfly@users.noreply.github.com>
2024-12-01 13:22:29 +0700

    migrate sertifikat

--- database/migrations/2024_11_30_162057_create_sertifikats_table.php
+++ database/migrations/2024_11_30_162057_create_sertifikats_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('sertifikats', function (Blueprint $table) {
             $table->id();
+            $table->string('sertifikat');
             $table->timestamps();
         });
     }
